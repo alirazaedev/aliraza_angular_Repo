e6d88491e882e17cf364bb2e58e86e784d071959
alirazaedev <ali.raza@edevtech.com>
2022-06-15 18:24:55 +0500

    1 file modified, 3 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { TopBarComponent } from './top-bar/top-bar.component';
 import { ProductListComponent } from './product-list/product-list.component';
+import { ProductAlertsComponent } from './product-alerts/product-alerts.component';
 
 @NgModule({
   imports: [
@@ -20,6 +21,7 @@ import { ProductListComponent } from './product-list/product-list.component';
     HelloComponent,
     TopBarComponent,
     ProductListComponent,
+    ProductAlertsComponent,
   ],
   bootstrap: [AppComponent],
 })
